2d117ee295dc124b4d86e124addf2c7c9e052f5f
une <thenum6er078@gmail.com>
2024-05-09 23:26:10 +0800

    Update(GNU Emacs): The True EDITOR📘

--- emacs/elisp/ui/theme/theme-dark.el
+++ emacs/elisp/ui/theme/theme-dark.el
@@ -10,10 +10,24 @@
   :init
   (load-theme 'ample t t)
 
-  ;; 在当天时间18:01~07:59时，自动切换到暗色主题
+  ;; 在当天时间18:01~22:59时，自动切换到暗色主题
   (run-at-time "18:00" (* 60 60 24) (lambda () 
                                       (disable-theme  'ample-flat)
                                       (enable-theme   'ample))))
+
+(use-package bliss-theme
+  :defer t
+  :ensure t
+  :init
+  (load-theme 'bliss t t)
+
+  ;; 在当天时间23:00~07:59时，自动切换到暗色主题
+  (run-at-time "23:00" (* 60 60 24) (lambda ()
+                                      (disable-theme  'nano-light)
+                                      (disable-theme  'ample-flat)
+                                      (disable-theme  'ample)
+                                      (enable-theme   'bliss))))
+
 
 (provide 'theme-dark)
 
