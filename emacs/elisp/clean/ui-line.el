44be8d8a5f5c7585114d36c27066f3681ceaa686
une <thenum6er078@gmail.com>
2024-10-05 23:39:54 +0800

    add(LaTeX desc-doc)

---
+++ emacs/elisp/clean/ui-line.el
@@ -0,0 +1,11 @@
+;; 
+;; ui-line.el 下面那条bar
+;; 
+
+(use-package telephone-line
+  :ensure t
+  :init
+  (setq telephone-line-primary-left-separator       'telephone-line-identity-left)
+  (telephone-line-mode 1))
+
+(provide 'ui-line)
