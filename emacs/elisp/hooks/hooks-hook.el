dea00c26b9acc9a0b1f0c0b5a64aabba7a476eb2
une <thenum6er078@gmail.com>
2024-10-26 22:21:28 +0800

    Update(GNU Emacs conf file)🏅.
    + 重量级更新(from chatGPT 4o): 在org-table下使用cdlatex按下TAB避免覆盖单元格，并添加了原问题发生原因的注释

--- emacs/elisp/hooks/hooks-hook.el
+++ emacs/elisp/hooks/hooks-hook.el
@@ -6,7 +6,7 @@
 ;; ===============================================
 ;; 编程类钩子hook
 ;; ===============================================
-(add-hook 'prog-mode 'hs-minor-mode)                        ; 折叠展开
+(add-hook 'prog-mode 'hs-minor-mode)                                         ; 折叠展开
 
 
 ;; ===============================================
@@ -16,21 +16,21 @@
     (when (get-buffer "*scratch*")
       (kill-buffer "*scratch*") 
     (when (get-buffer "*Messages*") 
-      (kill-buffer "*Messages*")))))                        ; 禁用Messages与scratch
-
-
-;; ===============================================
-;; 笔记文本org/LaTeX/text的钩子hook
-;; ===============================================
-(add-hook 'text-mode-hook 'turn-on-visual-line-mode)        ; 单行文本超出一定长度后自动虚拟换行显示(类似于set wrap)
-(add-hook 'TeX-after-compilation-finished-functions         ; AUCTeX(14.0.3.2024-03-17)
-          #'TeX-revert-document-buffer)
-(add-hook 'org-mode-hook #'org-cdlatex-mode)                ; 在org-mode使用OCDL(这是OCDL而不是CDL)
-(add-hook 'LaTeX-mode-hook #'cdlatex-mode)                  ; 在LaTeX-mode使用OCDL(这是OCDL而不是CDL)
-(add-hook 'org-mode-hook #'valign-mode)                     ; 在org-mode使用valign对齐不等宽字体
-(add-hook 'LaTeX-mode-hook #'tree-sitter-mode)              ; 在LaTeX-mode使用ts
+      (kill-buffer "*Messages*")))))                                         ; 禁用Messages与scratch
+                                                                            
+                                                                            
+;; ===============================================                          
+;; 笔记文本org/LaTeX/text的钩子hook                                         
+;; ===============================================                          
+(add-hook 'text-mode-hook 'turn-on-visual-line-mode)                         ; 单行文本超出一定长度后自动虚拟换行显示(类似于set wrap)
+(add-hook 'TeX-after-compilation-finished-functions                          ; AUCTeX(14.0.3.2024-03-17)
+          #'TeX-revert-document-buffer)                                     
+(add-hook 'org-mode-hook #'org-cdlatex-mode)                                 ; 在org-mode使用OCDL(这是OCDL而不是CDL)
+(add-hook 'LaTeX-mode-hook #'cdlatex-mode)                                   ; 在LaTeX-mode使用OCDL(这是OCDL而不是CDL)
+(add-hook 'org-mode-hook #'valign-mode)                                      ; 在org-mode使用valign对齐不等宽字体
+(add-hook 'LaTeX-mode-hook #'tree-sitter-mode)                               ; 在LaTeX-mode使用ts
 (add-hook 'org-mode-hook
           (lambda ()
-            (add-hook 'post-command-hook 'custom-org-table-with-cdlatex)))
+            (add-hook 'post-command-hook 'custom-org-table-with-cdlatex)))   ; 仅在org-table上下文范围内使用cdlatex的TAB来补全来防止单元格内容不会因org-cycle而被新插入的字符org-self-insert-command而覆盖
 
 (provide 'hooks-hook)
