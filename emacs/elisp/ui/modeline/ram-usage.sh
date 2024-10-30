1f3cf193a4ca02c4ef0914512261eea85ae62a6b
une <thenum6er078@gmail.com>
2024-10-30 23:25:44 +0800

    Update(conf file).

--- emacs/elisp/ui/modeline/ram-usage.sh
+++ emacs/elisp/ui/modeline/ram-usage.sh
@@ -1,6 +1,6 @@
 ram_usage=$(free -h | grep Gi | sed -n '1p' | cut -c '30-31')
 
-if [ "$ram_usage" -gt 8 ]; then
+if [ "$ram_usage" -gt 7 ]; then
     echo "$(free -h | grep Mi | sed -n '1p' | cut -c '31-33')M"
 else
     echo "$(free -h | grep Mi | sed -n '1p' | cut -c '31-33')G"
