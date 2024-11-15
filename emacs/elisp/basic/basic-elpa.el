55d314a9e954ce9d37a87477758761023a36ac6d
une <thenum6er078@gmail.com>
2024-11-15 17:21:31 +0800

    Update(GNU Emacs conf file)
    + 添加了eat终端模拟器
    
    + 修改了一些关于快捷键的说明

--- emacs/elisp/basic/basic-elpa.el
+++ emacs/elisp/basic/basic-elpa.el
@@ -4,8 +4,9 @@
 (use-package package
   :init 
   (setq package-archives 
-        '(("melpa" . "https://melpa.org/packages/")
-          ("gnu" . "https://elpa.gnu.org/packages/")))
+        '(("melpa" . "https://melpa.org/packages/")         
+          ("gnu" . "https://elpa.gnu.org/packages/")
+          ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
   (setq package-enable-at-startup nil)
   (setq use-package-expand-minimally t
         use-package-verbose t)
--- emacs/init.el
+++ emacs/init.el
@@ -30,6 +30,7 @@
 (require 'enhance-smartparens)
 (require 'enhance-tabs)
 (require 'enhance-rime)
+(require 'enhance-term)
 (require 'ui-font-sauce)
 (require 'ui-icon-nerdfont)
 (require 'ui-theme-light)
@@ -62,12 +63,7 @@
    '("1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
      "3001510be1be4a38508ff07633e97b810c0a52c6fa2f4c7153d22cc7596d9172"
      "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" default))
- '(package-selected-packages
-   '(auctex bliss-theme cdlatex centaur-tabs corfu dashboard deadgrep dirvish
-            embark-consult highlight-indent-guides keycast magit marginalia
-            nano-theme nerd-icons-corfu nerd-icons-dired orderless org-roam-ui
-            pdf-tools pkg-info restart-emacs rime smartparens telega
-            tree-sitter-langs valign vertico yasnippet-snippets)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
