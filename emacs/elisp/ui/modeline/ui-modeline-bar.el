90d555581508670c4b063086ded2388aec39c531
une <thenum6er078@gmail.com>
2024-11-17 11:59:18 +0800

    Sync(conf file).

--- emacs/elisp/ui/modeline/ui-modeline-bar.el
+++ emacs/elisp/ui/modeline/ui-modeline-bar.el
@@ -82,7 +82,7 @@
               modeline-output-pacman-packages
               "   "
               modeline-output-weather-situation
-              "  ")))
+              " ")))
 
 
 ;; ========================================
