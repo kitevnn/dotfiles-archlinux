254751c78df825063ca76de4ab96d33f46844c85
une <thenum6er078@gmail.com>
2024-11-21 13:37:49 +0800

    Sync.

--- emacs/elisp/basic/basic-path.el
+++ emacs/elisp/basic/basic-path.el
@@ -77,12 +77,6 @@
 (add-to-list 'load-path
              (expand-file-name 
               (concat user-emacs-directory "elisp/im")))
-;; =======================================
-;; 关于site-lisp设置
-;; =======================================
-(add-to-list 'load-path
-             (expand-file-name 
-              (concat user-emacs-directory "elisp/sitelisp")))
 
 
 (provide 'basic-path)
