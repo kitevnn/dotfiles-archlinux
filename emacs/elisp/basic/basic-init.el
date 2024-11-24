afd2f5b7608345565d8b0de4a35e737f80aad59f
une <thenum6er078@gmail.com>
2024-11-24 10:44:30 +0800

    Sync(conf file).
    + 设置了ibuffer的MRL Name长度

--- emacs/elisp/basic/basic-init.el
+++ emacs/elisp/basic/basic-init.el
@@ -29,6 +29,15 @@
 
 
 ;; =======================================
+;; ibuffer格式设置
+;; =======================================
+(setq ibuffer-formats
+      '((mark modified read-only locked " " (name 30 18 :left :elide) " "
+              (size 9 -1 :right) " " (mode 16 16 :left :elide) " " filename-and-process)
+        (mark " " (name 16 -1) " " filename)))
+
+
+;; =======================================
 ;; 起始目录
 ;; =======================================
 (setq default-directory "~/桌面/back/archlinux/check")             ; GNU Emacs的起始目录
