40cd91ec8e9a19905dbfb646c7d0519a4b38435e
une <thenum6er078@gmail.com>
2025-01-12 12:58:57 +0800

    Update(GNU Emacs conf file)
    + 更改了一些LaTeX快速指令，使得表达更规范。

--- emacs/init.el
+++ emacs/init.el
@@ -61,12 +61,7 @@
    '("1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
      "3001510be1be4a38508ff07633e97b810c0a52c6fa2f4c7153d22cc7596d9172"
      "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" default))
- '(package-selected-packages
-   '(auctex bliss-theme cdlatex centaur-tabs cmake-mode corfu dashboard deadgrep
-            dirvish eat embark-consult highlight-indent-guides magit marginalia
-            nano-theme nerd-icons-corfu nerd-icons-dired orderless org-roam-ui
-            pdf-tools pkg-info rainbow-identifiers restart-emacs smartparens
-            telega tree-sitter-langs valign vertico yasnippet-snippets)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
