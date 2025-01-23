47aedd3c65fa3a1a4aa8bf82da090f34de482aec
une <thenum6er078@gmail.com>
2025-01-23 18:13:58 +0800

    Update.

--- all/emacs/elisp/key/key-keybindings.el
+++ all/emacs/elisp/key/key-keybindings.el
@@ -106,19 +106,17 @@
 (define-key org-mode-map (kbd "C-z C-x C-\]")            'org-next-item)                              ; 跳转到下一个 (1) 2. 3) 的小标题
 (define-key org-mode-map (kbd "C-z C-z C-\\")            'org-agenda)                                 ; 打开org议题界面
 (define-key org-mode-map (kbd "C-z C-a C-\\")            'org-agenda)                                 ; 打开org议题界面
-(define-key org-mode-map (kbd "C-z C-a C-\[")            'org-agenda-file-to-front)                   ; 当前文件放进org议题内
-(define-key org-mode-map (kbd "C-z C-a C-\]")            'org-remove-file)                            ; 当前文件移出org议题外
-(define-key org-mode-map (kbd "C-z C-a C-s")             'org-schedule)                               ; 添加org议题的起始时间
-(define-key org-mode-map (kbd "C-z C-a C-d")             'org-deadline)                               ; 添加org议题的终止时间
+(define-key org-mode-map (kbd "C-z C-a C-\[")            'org-schedule)                               ; 添加org议题的起始时间
+(define-key org-mode-map (kbd "C-z C-a C-\]")            'org-deadline)                               ; 添加org议题的终止时间
 (define-key org-mode-map (kbd "C-z C-a C-\=")            'org-timestamp)                              ; 添加org议题的<>激活标签
 (define-key org-mode-map (kbd "C-z C-a C-\-")            'org-timestamp-inactive)                     ; 添加org议题的[]非激活标签
 (define-key org-mode-map (kbd "C-z C-a C-j")             'org-todo)                                   ; 切换todo/done/etc.标题文字
 (define-key org-mode-map (kbd "C-z C-a C-t")             'org-set-tags-command)                       ; 打上特征标签
 (define-key org-mode-map (kbd "C-z C-a C-0")             'org-archive-subtree)                        ; 删除subtree并保存至同目录下的org-archive文件内
-(define-key org-mode-map (kbd "C-z C-z C-\m")            'custom-update-modeline-all-information)           ; 手动更新modeline上的已有信息
+(define-key org-mode-map (kbd "C-z C-a C-\m")            'custom-update-modeline-all-information)     ; 手动更新modeline上的已有信息
 (define-key org-mode-map (kbd "C-z C-a C-w")             'custom-org-agenda-change-headline-to-wait)  ; 将当前任务状态改为WAIT
 (define-key org-mode-map (kbd "C-z C-a C-i")             'custom-org-agenda-change-headline-to-doing) ; 将当前任务状态改为DOING
-(define-key org-mode-map (kbd "C-z C-a C-o")             'custom-org-agenda-change-headline-to-done)  ; 将当前任务状态改为DONE
+(define-key org-mode-map (kbd "C-z C-a C-d")             'custom-org-agenda-change-headline-to-done)  ; 将当前任务状态改为DONE
 (global-unset-key (kbd "C-,"))                                                                        ; 屏蔽C-, (原功能: 直接进入org-agenda-files)
 (global-set-key (kbd "C-z C-a C-,")                      'org-cycle-agenda-files)                     ; 直接进入org-agenda-files
 
