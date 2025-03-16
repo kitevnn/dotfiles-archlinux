08909ba9f42c0caf68f04bbb6fde29e1201fb794
une <thenum6er078@gmail.com>
2025-03-16 22:33:04 +0800

    更新了 GNU Emacs 配置文件
    + 添加了一处文本说明

--- all/emacs/elisp/note/note-orgmode.el
+++ all/emacs/elisp/note/note-orgmode.el
@@ -65,7 +65,7 @@
 ;; ========================================
 ;; 设置org-download
 ;; ========================================
-;; 依赖xclip，同时截图工具如(spectacle)必须设置为复制截图到剪贴板或按下C-c
+;; 依赖xclip，同时截图工具如(spectacle)必须设置为复制截图到剪贴板或按下C-c来复制
 (use-package org-download
              :ensure t
              :defer nil
