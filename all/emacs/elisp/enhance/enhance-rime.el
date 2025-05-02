ada8beae6370b32db52d8b9063db82de74cef575
kle <thenum6er078@gmail.com>
2025-05-02 20:21:09 +0800

    更新了 GNU Emacs 配置文件
    + 添加了emacs-rime

---
+++ all/emacs/elisp/enhance/enhance-rime.el
@@ -0,0 +1,11 @@
+;; =======================================
+;; emacs-rime输入法 enhance-rime.el
+;; =======================================
+(use-package rime
+  :ensure t
+  :defer nil
+  :custom
+  (default-input-method "rime"))
+
+
+(provide 'enhance-rime)
